ba35e5985c93b497889a8123502235f76a3026bc
Hollow555ol <wolverine555ol@gmail.com>
2022-06-12 14:51:15 +0300

    implement input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const consolelog = () => {
+    console.log(text);
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={text} onChange={onChange} type="text"></input>
+
+      <button onClick={consolelog}>Add</button>
     </div>
   );
 }
